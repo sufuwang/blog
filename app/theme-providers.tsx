26e01d3ccfe692157258ff25e8440bba3dba2edf
sufuwang <1137120948@qq.com>
2024-12-18 20:34:51 +0800

    docs: work of introduce

--- app/theme-providers.tsx
+++ app/theme-providers.tsx
@@ -4,29 +4,24 @@ import { ThemeProvider } from 'next-themes'
 import siteMetadata from '@/data/siteMetadata'
 import { AntdRegistry } from '@ant-design/nextjs-registry'
 import { ConfigProvider, theme } from 'antd'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import ThemeContext, { Theme } from './theme-context'
+
+const Themes = {
+  light: theme.defaultAlgorithm,
+  dark: theme.darkAlgorithm,
+  system: theme.compactAlgorithm,
+}
 
 export function ThemeProviders({ children }: { children: React.ReactNode }) {
   const [curTheme, setTheme] = useState<Theme>(siteMetadata.theme)
 
-  useEffect(() => {
-    const t = localStorage.getItem('theme') as Theme
-    if (t) {
-      setTheme(t)
-    }
-  }, [])
-
   return (
     <ThemeContext.Provider value={{ theme: curTheme, setTheme }}>
       <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme} enableSystem>
-        <ConfigProvider
-          theme={{
-            algorithm: curTheme === 'dark' ? theme.darkAlgorithm : theme.defaultAlgorithm,
-          }}
-        >
-          <AntdRegistry>{children}</AntdRegistry>
-        </ConfigProvider>
+        <AntdRegistry>
+          <ConfigProvider theme={{ algorithm: Themes[curTheme] }}>{children}</ConfigProvider>
+        </AntdRegistry>
       </ThemeProvider>
     </ThemeContext.Provider>
   )
