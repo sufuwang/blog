03135f3f7b138cc779e69d12e5b6c439ab15c40c
sufuwang <1137120948@qq.com>
2024-12-19 15:42:23 +0800

    feat: CountDown of introduce

---
+++ components/Blog/CountDown.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import dayjs from 'dayjs'
+
+const calc = () => {
+  const work = dayjs().diff(dayjs('2021.7.1'), 'days')
+  const birthday = dayjs().diff(dayjs('1995.8.18'), 'days')
+  return [
+    `已生活 ${Math.floor(birthday / 365)} 年 ${Math.floor(birthday % 365)} 天`,
+    `已工作 ${Math.floor(work / 365)} 年 ${Math.floor(work % 365)} 天`,
+  ].join(' / ')
+}
+
+const CountDown = () => {
+  const [str, setStr] = useState(calc())
+
+  useEffect(() => {
+    const interval = setInterval(
+      () => {
+        setStr(calc())
+      },
+      1000 * 60 * 5
+    )
+
+    return () => clearInterval(interval)
+  }, [])
+
+  return <span>{str}</span>
+}
+
+export default CountDown
--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -2,10 +2,11 @@ import TOCInline from 'pliny/ui/TOCInline'
 import Pre from 'pliny/ui/Pre'
 import BlogNewsletterForm from 'pliny/ui/BlogNewsletterForm'
 import type { MDXComponents } from 'mdx/types'
+import { Descriptions, Badge, Collapse, Table } from 'antd'
 import Image from './Image'
 import CustomLink from './Link'
 import TableWrapper from './TableWrapper'
-import { Descriptions, Badge, Collapse, Table } from 'antd'
+import CountDown from './Blog/CountDown'
 
 export const components: MDXComponents = {
   Image,
@@ -18,4 +19,5 @@ export const components: MDXComponents = {
   Badge,
   Collapse,
   Table,
+  CountDown,
 }
