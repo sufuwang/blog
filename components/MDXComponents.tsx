8e91c0fc02932d68c94736023630050bde17f53a
sufuwang <1137120948@qq.com>
2025-01-17 09:03:51 +0800

    docs: my project of introduce

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -2,7 +2,7 @@ import TOCInline from 'pliny/ui/TOCInline'
 import Pre from 'pliny/ui/Pre'
 import BlogNewsletterForm from 'pliny/ui/BlogNewsletterForm'
 import type { MDXComponents } from 'mdx/types'
-import { Descriptions, Badge, Collapse, Table } from 'antd'
+import { Descriptions, Badge, Collapse, Table, Tag } from 'antd'
 import Image from './Image'
 import CustomLink from './Link'
 import TableWrapper from './TableWrapper'
@@ -19,5 +19,6 @@ export const components: MDXComponents = {
   Badge,
   Collapse,
   Table,
+  Tag,
   CountDown,
 }
