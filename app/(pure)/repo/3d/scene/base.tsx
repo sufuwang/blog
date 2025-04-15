2b362a5a16a3d7fbc6120b648eabdbf5d3ea66f1
sufuwang <1137120948@qq.com>
2025-04-15 15:54:03 +0800

    feat: gui

--- app/(pure)/repo/3d/scene/base.tsx
+++ app/(pure)/repo/3d/scene/base.tsx
@@ -2,6 +2,7 @@
 import { useEffect } from 'react'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls'
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js'
 
 export default function RepoPage() {
   const scene = new THREE.Scene()
@@ -38,6 +39,43 @@ export default function RepoPage() {
 
     document.body.appendChild(renderer.domElement)
     new OrbitControls(camera, renderer.domElement)
+
+    const gui = new GUI()
+
+    const meshFolder = gui.addFolder('mesh')
+    meshFolder.addColor(mesh.material, 'color')
+    meshFolder.add(mesh.position, 'x').step(10)
+    meshFolder.add(mesh.position, 'y').step(10)
+    meshFolder.add(mesh.position, 'z').step(10)
+
+    const lightFolder = gui.addFolder('light')
+    lightFolder.add(pointLight.position, 'x').step(10)
+    lightFolder.add(pointLight.position, 'y').step(10)
+    lightFolder.add(pointLight.position, 'z').step(10)
+    lightFolder.add(pointLight, 'intensity').step(1000)
+
+    const otherFolder = gui.addFolder('other')
+    const obj = {
+      input: '天王盖地虎',
+      checkbox: false,
+      slider: 0,
+      selectA: '111',
+      selectB: 'Bbb',
+      logic: function () {
+        alert('执行一段逻辑!')
+      },
+    }
+    otherFolder
+      .add(obj, 'input')
+      .name('输入框')
+      .onChange((value) => {
+        console.log(value)
+      })
+    otherFolder.add(obj, 'checkbox').name('勾选框')
+    otherFolder.add(obj, 'slider').name('滑块').min(-10).max(10).step(0.5)
+    otherFolder.add(obj, 'selectA', ['111', '222', '333']).name('下拉框 A')
+    otherFolder.add(obj, 'selectB', { Aaa: 0, Bbb: 0.1, Ccc: 5 }).name('下拉框 B')
+    otherFolder.add(obj, 'logic').name('按钮')
 
     function r() {
       renderer.render(scene, camera)
