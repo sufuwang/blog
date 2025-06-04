0ecd5f8c70f1f5cf80db1a3369a3b673be9c8395
sufuwang <1137120948@qq.com>
2025-06-04 14:50:15 +0800

    feat: Box3 APIs

--- app/(pure)/repo/3d/scene/gltf.tsx
+++ app/(pure)/repo/3d/scene/gltf.tsx
@@ -33,19 +33,25 @@ export default function InfiniteTube() {
     }
     {
       const loader = new GLTFLoader()
-      loader.load('/model/cow/Cow.gltf', function (gltf) {
+      loader.load('/model/cow/Cow.gltf', async function (gltf) {
         gltf.scene.position.x = 10
         scene.add(gltf.scene)
 
         const helper = new THREE.BoxHelper(gltf.scene, 'pink')
         scene.add(helper)
 
-        const box = new THREE.Box3()
-        box.expandByObject(gltf.scene)
-        const width = box.max.x - box.min.x
-        const height = box.max.y - box.min.y
-        const depth = box.max.z - box.min.z
-        console.log('Box3:', { width, height, depth })
+        const cowBox = new THREE.Box3()
+        // box.expandByObject(gltf.scene)
+        cowBox.setFromObject(gltf.scene)
+        const width = cowBox.max.x - cowBox.min.x
+        const height = cowBox.max.y - cowBox.min.y
+        const depth = cowBox.max.z - cowBox.min.z
+        const size = new THREE.Vector3()
+        cowBox.getSize(size)
+        console.info('cowBox size:', { width, height, depth }, size)
+        // cowBox.expandByScalar(2)
+        const helper3 = new THREE.Box3Helper(cowBox, 'red')
+        scene.add(helper3)
 
         const ringGeometry = new THREE.RingGeometry(depth / 2, depth / 2 + 0.4)
         const ringMaterial = new THREE.MeshBasicMaterial({
@@ -56,14 +62,35 @@ export default function InfiniteTube() {
         ring.position.y = height / 2
         ring.rotateX(Math.PI / 2)
         gltf.scene.add(ring)
+
+        const michelle = await loader.loadAsync('/model/michelle/Michelle.glb')
+        scene.add(michelle.scene)
+        const michelleBox = new THREE.Box3()
+        michelleBox.setFromObject(michelle.scene)
+        // michelleBox.expandByScalar(8)
+        const michelleBoxHelper = new THREE.Box3Helper(michelleBox, 'blue')
+        scene.add(michelleBoxHelper)
+        console.info('Michelle & Cow 是否碰撞: ', michelleBox.intersectsBox(cowBox))
+        // const intersectBox = michelleBox.intersect(cowBox)
+        // const intersectSize = intersectBox.getSize(new THREE.Vector3())
+        // console.info('Michelle & Cow 碰撞尺寸: ', intersectSize)
+
+        const unionBox = michelleBox.union(cowBox)
+        const helper4 = new THREE.Box3Helper(unionBox, 'green')
+        scene.add(helper4)
+        const unionSize = unionBox.getSize(new THREE.Vector3())
+        console.info('Michelle & Cow 并集尺寸', unionSize)
+
+        const michelleBoxCenter = michelleBox.getCenter(new THREE.Vector3())
+        console.info('Michelle Box Center:', michelleBoxCenter)
       })
     }
 
     const ambientLight = new THREE.AmbientLight()
     scene.add(ambientLight)
 
-    // const axesHelper = new THREE.AxesHelper(200)
-    // scene.add(axesHelper)
+    const axesHelper = new THREE.AxesHelper(200)
+    scene.add(axesHelper)
 
     const width = window.innerWidth
     const height = window.innerHeight
