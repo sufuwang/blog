37b95590e19ae7541c0ff1b5fce90dcefb6227e7
sufuwang <1137120948@qq.com>
2025-06-12 10:51:08 +0800

    feat: reflect objects by Reflector

--- app/(pure)/repo/3d/scene/rainbowBubble.tsx
+++ app/(pure)/repo/3d/scene/rainbowBubble.tsx
@@ -6,15 +6,15 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 // @ts-ignore
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js'
-import { RGBELoader } from 'three/examples/jsm/Addons.js'
+import { Reflector, RGBELoader } from 'three/examples/jsm/Addons.js'
 
 export default function RainbowBubble() {
   useEffect(() => {
     const scene = new THREE.Scene()
     // const loader = new GLTFLoader()
     const gui = new GUI()
-    const cubeRenderTarget = new THREE.WebGLCubeRenderTarget(512)
-    const cubeCamera = new THREE.CubeCamera(1, 1000, cubeRenderTarget)
+    const textureWidth = window.innerWidth * window.devicePixelRatio
+    const textureHeight = window.innerHeight * window.devicePixelRatio
 
     // {
     //   const textureLoader = new THREE.CubeTextureLoader()
@@ -52,17 +52,42 @@ export default function RainbowBubble() {
 
         {
           const geometry = new THREE.PlaneGeometry(1000, 1000)
-          const material = new THREE.MeshStandardMaterial({
-            color: 'white',
-            metalness: 1,
-            roughness: 0,
-            // envMap: texture,
-            envMap: cubeRenderTarget.texture,
-          })
-          const mesh = new THREE.Mesh(geometry, material)
-          mesh.position.set(0, 0, -800)
-          mesh.rotateY(Math.PI)
+          // const material = new THREE.MeshStandardMaterial({
+          //   color: 'white',
+          //   metalness: 1,
+          //   roughness: 0,
+          //   // envMap: texture,
+          //   side: THREE.DoubleSide,
+          //   // envMap: cubeRenderTargetA.texture,
+          // })
+          // const mesh = new THREE.Mesh(geometry, material)
+          // mesh.position.set(0, 0, 400)
+          // mesh.rotateY(Math.PI * 0.1)
+          // mesh.name = 'plane'
+          const mesh = new Reflector(geometry, { textureWidth, textureHeight })
+          mesh.position.set(0, 0, 400)
+          mesh.rotateY(Math.PI * 1.1)
           mesh.name = 'plane'
+          scene.add(mesh)
+        }
+        {
+          const geometry = new THREE.PlaneGeometry(1000, 1000)
+          // const material = new THREE.MeshStandardMaterial({
+          //   color: 'white',
+          //   metalness: 1,
+          //   roughness: 0,
+          //   // envMap: texture,
+          //   side: THREE.DoubleSide,
+          //   // envMap: cubeRenderTargetB.texture,
+          // })
+          // const mesh = new THREE.Mesh(geometry, material)
+          // mesh.position.set(-400, 0, 0)
+          // mesh.rotateY(Math.PI * 0.4)
+          // mesh.name = 'plane2'
+          const mesh = new Reflector(geometry, { textureWidth, textureHeight })
+          mesh.position.set(-400, 0, 0)
+          mesh.rotateY(Math.PI * 0.4)
+          mesh.name = 'plane2'
           scene.add(mesh)
         }
         {
@@ -71,7 +96,6 @@ export default function RainbowBubble() {
             color: 'green',
           })
           const mesh = new THREE.Mesh(geometry, material)
-          mesh.position.set(0, 0, -1200)
           mesh.name = 'sphere'
           scene.add(mesh)
         }
@@ -87,13 +111,12 @@ export default function RainbowBubble() {
           iridescenceIOR: 1.8,
           reflectivity: 1,
         })
-
         gui.addColor(material, 'color')
         gui.add(material, 'iridescence', 0, 1)
         gui.add(material, 'iridescenceIOR', 1, 2.33)
         gui.add(material, 'reflectivity', 0, 1)
-
         const mesh = new THREE.Mesh(geometry, material)
+        mesh.position.set(0, 1600, 0)
         scene.add(mesh)
       })
     }
@@ -123,13 +146,23 @@ export default function RainbowBubble() {
     })
     renderer.setSize(width, height)
 
-    const render = () => {
-      const t = scene.getObjectByName('plane')
-      if (t) {
-        cubeCamera.position.copy(t.position)
-      }
+    // const cubeRenderTargetA = new THREE.WebGLCubeRenderTarget(512)
+    // const cubeCameraA = new THREE.CubeCamera(1, 1000, cubeRenderTargetA)
+    // const cubeRenderTargetB = new THREE.WebGLCubeRenderTarget(512)
+    // const cubeCameraB = new THREE.CubeCamera(1, 1000, cubeRenderTargetB)
 
-      cubeCamera.update(renderer, scene)
+    const render = () => {
+      // const plane = scene.getObjectByName('plane')
+      // if (plane) {
+      //   cubeCameraA.position.copy(plane.position)
+      // }
+      // const plane2 = scene.getObjectByName('plane2')
+      // if (plane2) {
+      //   cubeCameraB.position.copy(plane2.position)
+      // }
+      // cubeCameraA.update(renderer, scene)
+      // cubeCameraB.update(renderer, scene)
+
       renderer.render(scene, camera)
       requestAnimationFrame(render)
     }
