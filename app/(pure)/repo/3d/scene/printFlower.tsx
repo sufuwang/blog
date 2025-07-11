d4644f84713fd03bb13a4c2c4f3e3dccc0ab13bf
sufuwang <1137120948@qq.com>
2025-07-11 10:45:56 +0800

    feat: download image & video from 3d scene

--- app/(pure)/repo/3d/scene/printFlower.tsx
+++ app/(pure)/repo/3d/scene/printFlower.tsx
@@ -4,7 +4,8 @@ import * as THREE from 'three'
 // @ts-ignore
 import { OrbitControls } from 'three/addons/controls/OrbitControls'
 import { DecalGeometry, DRACOLoader, GLTFLoader } from 'three/examples/jsm/Addons'
-import { ColorPicker } from 'antd'
+import { FileImageOutlined, VideoCameraOutlined } from '@ant-design/icons'
+import { FloatButton, ColorPicker } from 'antd'
 
 export default function PrintFlower() {
   const loader = new THREE.TextureLoader()
@@ -14,12 +15,49 @@ export default function PrintFlower() {
   dracoLoader.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.5.6/')
   gltfLoader.setDRACOLoader(dracoLoader)
   const scene = new THREE.Scene()
-  const renderer = new THREE.WebGLRenderer()
+  const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+  })
+  let orbitControls: OrbitControls = null
 
   const onColorChange = (_, color) => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const tshirt = scene.getObjectByName('tshirt') as unknown as any
     tshirt?.children[0].material.color.set(color)
+  }
+  const downloadBlob = (blob, filename) => {
+    const url = URL.createObjectURL(blob)
+    const a = document.createElement('a')
+    a.href = url
+    a.download = filename
+    document.body.appendChild(a)
+    a.click()
+    document.body.removeChild(a)
+    URL.revokeObjectURL(url)
+  }
+  const onDownloadImage = () => {
+    renderer.domElement.toBlob((blob) => {
+      if (blob) {
+        downloadBlob(blob, 't-shirt.png')
+      }
+    }, 'image/png')
+  }
+  const onDownloadVideo = () => {
+    const stream = renderer.domElement.captureStream(60)
+    const mediaRecorder = new MediaRecorder(stream, { mimeType: 'video/webm' })
+    mediaRecorder.start()
+    orbitControls.autoRotate = true
+    orbitControls.autoRotateSpeed = 8
+    mediaRecorder.ondataavailable = (event) => {
+      if (event.data.size > 0) {
+        downloadBlob(event.data, 't-shirt.webm')
+      }
+    }
+    setTimeout(() => {
+      orbitControls.autoRotate = false
+      orbitControls.reset()
+      mediaRecorder.stop()
+    }, 5000)
   }
 
   useEffect(() => {
@@ -38,8 +76,8 @@ export default function PrintFlower() {
     const ambientLight = new THREE.AmbientLight(0xffffff)
     scene.add(ambientLight)
 
-    const axesHelper = new THREE.AxesHelper(200)
-    scene.add(axesHelper)
+    // const axesHelper = new THREE.AxesHelper(200)
+    // scene.add(axesHelper)
 
     const width = window.innerWidth
     const height = window.innerHeight
@@ -71,18 +109,24 @@ export default function PrintFlower() {
     })
 
     document.body.appendChild(renderer.domElement)
-    new OrbitControls(camera, renderer.domElement)
+    orbitControls = new OrbitControls(camera, renderer.domElement)
 
     function r() {
+      orbitControls.update()
       renderer.render(scene, camera)
       requestAnimationFrame(r)
     }
     requestAnimationFrame(r)
+    onColorChange(null, '#FFF')
   }, [])
 
   return (
     <div className="absolute left-2 top-2">
-      <ColorPicker defaultValue="#1677ff" disabledAlpha onChange={onColorChange} />
+      <ColorPicker defaultValue="#FFF" disabledAlpha onChange={onColorChange} />
+      <FloatButton.Group shape="square">
+        <FloatButton icon={<FileImageOutlined />} onClick={onDownloadImage} />
+        <FloatButton icon={<VideoCameraOutlined />} onClick={onDownloadVideo} />
+      </FloatButton.Group>
     </div>
   )
 }
